eeb9dcca00c9a2a311cec0e0b703997c9dffdbe8
PRAKTIKAN <PRAKTIKAN@DESKTOP-MU6PA4D>
2025-04-17 10:26:21 +0700

    commit pertama

---
+++ BankTransferConfig.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace modul8_103022300143
+{
+    internal class BankTransferConfig
+    {
+        public string lang { get; set; }
+        public class transfer
+        {
+            public int threshold { get; set; }
+            public int low_fee { get; set; }
+            public int high_fee { get; set; }
+        }
+
+        public List<string> methods { get; set; }
+
+        public class confirmation
+        {
+            public string en { get; set; }
+            public string id { get; set; }
+        }
+
+        public BankTransferConfig ReadConfigFile()
+        {
+            String configJsonData = File.ReadAllText("D:\\modul8_103022300143\\bank_transfer_config.json");
+            config = JsonSerializer.Deserialize<BankTransferConfig>(configJsonData);
+            return config;
+        }
+    }
+}
