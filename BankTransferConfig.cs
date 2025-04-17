241255102f7d7b3b21cc772939479112333d30d9
PRAKTIKAN <PRAKTIKAN@DESKTOP-MU6PA4D>
2025-04-17 11:13:14 +0700

    aplikasi bank transfer dengan runtime configuration

--- BankTransferConfig.cs
+++ BankTransferConfig.cs
@@ -9,27 +9,60 @@ namespace modul8_103022300143
 {
     internal class BankTransferConfig
     {
-        public string lang { get; set; }
-        public class transfer
+        public class Config
         {
-            public int threshold { get; set; }
-            public int low_fee { get; set; }
-            public int high_fee { get; set; }
+            public string lang { get; set; }
+            public class transfer
+            {
+                public int threshold { get; set; }
+                public int low_fee { get; set; }
+                public int high_fee { get; set; }
+            }
+
+            public List<string> methods { get; set; }
+
+            public class confirmation
+            {
+                public string en { get; set; }
+                public string id { get; set; }
+            }
         }
 
-        public List<string> methods { get; set; }
-
-        public class confirmation
-        {
-            public string en { get; set; }
-            public string id { get; set; }
-        }
+        public Config config;
 
         public BankTransferConfig ReadConfigFile()
         {
             String configJsonData = File.ReadAllText("D:\\modul8_103022300143\\bank_transfer_config.json");
-            config = JsonSerializer.Deserialize<BankTransferConfig>(configJsonData);
-            return config;
+            return JsonSerializer.Deserialize<BankTransferConfig>(configJsonData);
         }
+        public void setdefault()
+        {
+            config = new Config();
+            config.lang = "en";
+        }
+
+        public void WriteNewConfigFile()
+        {
+            JsonSerializerOptions options = new JsonSerializerOptions()
+            {
+                WriteIndented = true
+            };
+            String jsonString = JsonSerializer.Serialize(config, options);
+            File.WriteAllText("D:\\modul8_103022300143\\bank_transfer_config.json", jsonString);
+        }
+
+        public BankTransferConfig()
+        {
+            try
+            {
+                ReadConfigFile();
+            }
+            catch (Exception)
+            {
+                setdefault();
+                WriteNewConfigFile();
+            }
+        }
+
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using modul8_103022300143;
+
+class Program
+{
+    static void Main()
+    {
+        BankTransferConfig config = new BankTransferConfig();
+        if(config.config.lang=="en"){
+            Console.WriteLine("Please insert the amount of money to transfer:");
+        }else if (config.config.lang == "id")
+        {
+            Console.WriteLine("Masukkan jumlah uang yang akan di-transfer:");
+        }
+    }
+}
